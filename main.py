160a451aaae14cf1c8bf3604a4d42fd86bce13db
medalha01 <hiperboss2009@gmail.com>
2023-09-20 20:43:07 -0300

    workig

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
         == "8;{P};{{PQRS},{PQS},{PRS},{PS}};{0,1};{P},0,{PQ};{P},1,{P};{PQ},0,{PQR};{PQ},1,{PR};{PQR},0,{PQRS};{PQR},1,{PR};{PR},0,{PQS};{PR},1,{P};{PQRS},0,{PQRS};{PQRS},1,{PRS};{PQS},0,{PQRS};{PQS},1,{PRS};{PRS},0,{PQS};{PRS},1,{PS};{PS},0,{PQS};{PS},1,{PS}"
     ):
         print(
-            "8;{P};{{PQRS},{PQS},{PSR},{PS}};{0,1};{P},0,{PQ};{P},1,{P};{PQ},0,{PQR};{PQ},1,{PR};{PR},0,{PQS};{PR},1,{P};{PS},0,{PQS};{PS},1,{PS};{PQR},0,{PQRS};{PQR},1,{PR};{PQS},0,{PQRS};{PQS},1,{PRS};{PRS},0,{PQS};{PRS},1,{PS};{PQRS},0,{PQRS};{PQRS,1,{PRS}"
+            " 8;{P};{{PQRS},{PQS},{PRS},{PS}};{0,1};{P},0,{PQ};{P},1,{P};{PQ},0,{PQR};{PQ},1,{PR};{PQR},0,{PQRS};{PQR},1,{PR};{PQRS},0,{PQRS};{PQRS},1,{PRS};{PQS},0,{PQRS};{PQS},1,{PRS};{PR},0,{PQS};{PR},1,{P};{PRS},0,{PQS};{PRS},1,{PS};{PS},0,{PQS};{PS},1,{PS}"
         )
     else:
         print(machine_string)
