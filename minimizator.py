03974ee9d36e42c56a62c65585a641a260594773
medalha01 <hiperboss2009@gmail.com>
2023-09-23 18:04:18 -0300

    AAAAAAAAAA

--- minimizator.py
+++ minimizator.py
@@ -6,7 +6,7 @@ class MinimizationAlgorithm:
     def __init__(self, machine: Machine):
         self.machine = machine
         self.minimization_group = []
-        self.dead_group = MinimizationGroup(None, "dead", [])
+        self.dead_group = MinimizationGroup("dead", "dead", [])
         self.non_final_states = MinimizationGroup("non_final", "non_final", [])
         self.final_states = MinimizationGroup("final", "final", [])
         self.__init_groups()
@@ -28,15 +28,20 @@ class MinimizationAlgorithm:
     def __remove_unreachable(self):
         reacheable_states = [self.machine.starting_state]
         machine_alphabet = self.machine.get_alphabet()
-        for state in reacheable_states:
+        counter = 0
+        while counter < len(reacheable_states):
+            state = reacheable_states[counter]
             self.machine.set_current_state(state)
+            if state is None:
+                continue
             print(state.state_identifier)
-            for symbol in machine_alphabet:
+            for symbol in machine_alphabet.split(","):
                 list_of_states = self.machine.execute_machine_step(symbol)
-                for identifier in list_of_states:
-                    temporary_state = self.machine.get_state(identifier)
-                    if temporary_state not in reacheable_states:
-                        reacheable_states.append(temporary_state)
+                for temp_state in list_of_states:
+                    ##temporary_state = self.machine.get_state(identifier)
+                    if temp_state not in reacheable_states:
+                        reacheable_states.append(temp_state)
+            counter += 1
         temporary_state = self.machine.get_state("dead")
         if temporary_state not in reacheable_states:
             reacheable_states.append(temporary_state)
@@ -87,16 +92,20 @@ class MinimizationAlgorithm:
                         target_state_identifier = "dead"
                     else:
                         target_state = self.machine.get_state(target_state_identifier)
+                    print(group.group_id)
+                    print(group.target_group)
                     object_target_group = self.get_group_by_id(group.target_group)
                     if target_state not in object_target_group.state_list:
                         group.remove(state.state_identifier)
                         target_id = self.get_new_target(target_state)
                         for new_group in new_groups:
                             if new_group.target_group == target_id:
+                                print(state.state_identifier)
                                 new_group.append(state)
                                 group_not_found = False
                                 break
                         if group_not_found:
+                            print("state.state_identifier")
                             new_group = MinimizationGroup(target_id, counter, [state])
                             new_groups.append(new_group)
                             counter += 1
